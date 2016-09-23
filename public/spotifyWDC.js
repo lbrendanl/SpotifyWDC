28933703d7840db10e1a9eac182721b67d4e0999
lbrendanl <lee33@uw.edu>
2016-09-23 16:14:56 -0700

    temp

--- public/spotifyWDC.js
+++ public/spotifyWDC.js
@@ -1,82 +1,97 @@
+"use strict";
+
+var s, params, access_token, refresh_token, error;;
+
 // Define our Web Data Connector
 (function() {
-    var $ = require('jquery');
-    var _ = require('underscore');
-    var Spotify = require('spotify-web-api-js');
-    
-    var s = new Spotify(),
+    var myConnector = tableau.makeConnector();
+
+    myConnector.init = function(initCallback){
+        s = new SpotifyWebApi();
         params = getHashParams();
+        
         access_token = params.access_token,
         refresh_token = params.refresh_token,
         error = params.error;
+        
+        console.log("Platform Version: " + tableau.platformVersion);
 
-    if (error) {
-        alert('There was an error during the authentication');
-    } else {
-        if (access_token) {
-            console.log("logged in");
+        if (error) {
+            console.error("There was an error during the authentication");
         } else {
-            console.log("not logged in");
-        }
-    }
 
-    var myConnector = tableau.makeConnector();
+        }
+
+        if  (tableau.phase == tableau.phaseEnum.interactivePhase || tableau.phase == tableau.phaseEnum.authPhase) {
+            tableau.password = access_token;
+        }
+
+        initCallback();
+    };
 
     myConnector.getSchema = function(schemaCallback) {
-
+        $.getJSON("./schema.json", function(schemaJson) {
+            schemaCallback(schemaJson);
+        });
     }
 
     myConnector.getData = function(table, doneCallback) {
+        s.setAccessToken(tableau.password);
+        
+        s.getUserPlaylists()  // note that we don't pass a user id
+        .then(function(data) {
+            console.log('User playlists', data);
+        }, function(err) {
+            console.error(err);
+        });
 
+        s.getMyTopArtists().then(function(data) {
+            console.log("top tracks: ", data);
+        }, function(err) {
+            console.error(err);
+        });
     }
 
     tableau.registerConnector(myConnector);
-})();
 
+    //--------------------------------HELPERS---------------------------------
 
-
-//--------------------------------HELPERS---------------------------------
-
-/**
- * Obtains parameters from the hash of the URL
- * @return Object
- */
-function getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while (e = r.exec(q)) {
-        hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
-}
-
-// Note: Refresh tokens are valid forever, just need to get a new access token.
-// Refresh tokens can me manually revoked but won't expire
-function refreshToken() {
-    $.ajax({
-        url: '/refresh_token',
-        data: {
-            'refresh_token': refresh_token
-        }
-    }).done(function(data) {
-        access_token = data.access_token;
+    $(document).ready(function() {
+        $("#getdata").click(function() { // This event fires when a button is clicked
+            setupConnector();
+        });
     });
-}
 
-// Helper function that loads a json and a callback to call once that file is loaded
-function loadJSON(path, cb, isLocal) {
-    var obj = new XMLHttpRequest();
-    obj.overrideMimeType("application/json");
-    if (isLocal) {
-        obj.open("GET", "../json/" + path + ".json", true);
-    } else {
-        obj.open("GET", "https://crossorigin.me/http://jsonplaceholder.typicode.com/" + path, true);
-    }
-    obj.onreadystatechange = function() {
-        if (obj.readyState == 4 && obj.status == "200") {
-            cb(obj.responseText);
+    /**
+     * Obtains parameters from the hash of the URL
+     * @return Object
+     */
+    function getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        while (e = r.exec(q)) {
+            hashParams[e[1]] = decodeURIComponent(e[2]);
         }
+        return hashParams;
     }
-    obj.send(null);
-}
+
+
+    function setupConnector() {
+        tableau.connectionName = "Spotify Connector";
+        tableau.submit();
+    };
+
+    // Note: Refresh tokens are valid forever, just need to get a new access token.
+    // Refresh tokens can me manually revoked but won"t expire
+    function refreshToken() {
+        $.ajax({
+            url: "/refresh_token",
+            data: {
+                "refresh_token": refresh_token
+            }
+        }).done(function(data) {
+            access_token = data.access_token;
+        });
+    }
+})();
