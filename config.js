ff9434142673e3b0358b4980597804f2612cb345
sdesmond46 <sdesmond46@gmail.com>
2016-11-01 16:09:52 +0000

    Use hostname for config

--- config.js
+++ config.js
@@ -2,9 +2,16 @@
 // Contains credentials for your Spotify application
 // And the new redirect path for the OAuth flow
 // Should be kept secret
+
+var PORT = 3000;
+
+var os = require("os");
+var hostName = os.hostname();
+var redirectUri = "http://" + hostName + ":" + PORT + "/callback";
+
 module.exports = {
- 'PORT': 3000,
+ 'PORT': PORT,
  'CLIENT_ID': 'f0551d1b48d241b9a21d6264c828fd7b',
  'CLIENT_SECRET': '7931169be79e44a19a4d21e449e84a74',
- 'REDIRECT_URI': 'http://localhost:3000/callback'
+ 'REDIRECT_URI': redirectUri
 };
